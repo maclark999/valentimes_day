776ffd1d7ac4efcdece45bbb062af54a224a22d3
mackenzie clarkson <maclark99@gmail.com>
2015-02-12 16:32:11 -0700

    user can submit message, user can see show page

--- app/controllers/valentines_controller.rb
+++ app/controllers/valentines_controller.rb
@@ -1,3 +1,20 @@
 class ValentinesController < ApplicationController
-  
+
+  def new
+    @valentine = Valentine.new
+  end
+
+  def create
+    @valentine = Valentine.new(valentine_params)
+    if @valentine.save
+      redirect_to @valentine
+    else
+      render :new
+    end
+  end
+
+private
+  def valentine_params
+    params.require(:valentine).permit(:name, :message)
+  end
 end
